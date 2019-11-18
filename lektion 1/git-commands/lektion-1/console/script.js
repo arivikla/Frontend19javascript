ecc9d8570e626db82398548b424f8c04256deac1
Ari Vikla <ari.vikla@gmail.com>
2019-11-18 13:02:09 +0100

    lektion 1

---
+++ lektion 1/git-commands/lektion-1/console/script.js
@@ -0,0 +1,12 @@
+// en kommentar skrivs så här
+/* en kommentar kan skrivas så häs */
+
+/* märkspråk  skript språk  programmeringsspråk (kombileras)
+    HTML        JavaSript       C
+    CSS         PowerShell      C++
+    XML         cmdLets         C#
+                (python)        Java
+                                (python)
+
+
+*/
